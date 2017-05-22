7cf7cc11dc9510d193e942068b9b8fa92c996654
何加一 <JAY@oonne.com>
2017-05-22 11:05:54 +0800

    proportion data

--- backend/controllers/ChartController.php
+++ backend/controllers/ChartController.php
@@ -8,6 +8,8 @@ use yii\web\BadRequestHttpException;
 use yii\db\Query;
 use common\models\Expenses;
 use common\models\Income;
+use common\models\Category;
+use common\models\Handler;
 
 class ChartController extends Controller
 {
@@ -41,11 +43,53 @@ class ChartController extends Controller
         $expensesTotal = ($expensesQuery->createCommand()->queryOne())['summary'];
 
         // proportion data
-        // TODO
+        $expensesCategory = [];
+        $expensesCategoryQuery = (new Query())->select([
+                'category' => 'T0.expenses_category',
+                'value' => 'SUM(T0.expenses_money)'])
+            ->from(['T0' => Expenses::tableName()])
+            ->groupBy(['T0.expenses_category']);
+        $expensesCategoryResult = $expensesCategoryQuery->createCommand()->queryAll();
+
+        $categoryList = Category::getCategoryList();
+        foreach ($categoryList as $category) {
+            foreach ($expensesCategoryResult as $item)  
+            {
+                if($item['category'] == $category['id']){
+                    array_push($expensesCategory, [
+                        'name' => $category['category_name'],
+                        'value' => $item['value']
+                    ]);
+                }
+            }
+        }
+
+        $incomeHandler = [];
+        $incomeHandlerQuery = (new Query())->select([
+                'handler' => 'T0.income_handler',
+                'value' => 'SUM(T0.income_money)'])
+            ->from(['T0' => Income::tableName()])
+            ->groupBy(['T0.income_handler']);
+        $incomeHandlerResult = $incomeHandlerQuery->createCommand()->queryAll();
+
+        $handlerList = Handler::getHandlerList();
+        foreach ($handlerList as $handler) {
+            foreach ($incomeHandlerResult as $item)  
+            {
+                if($item['handler'] == $handler['id']){
+                    array_push($incomeHandler, [
+                        'name' => $handler['handler_name'],
+                        'value' => $item['value']
+                    ]);
+                }
+            }
+        }
 
         return $this->render('index', [
             'incomeTotal' => $incomeTotal,
             'expensesTotal' => $expensesTotal,
+            'expensesCategory' => $expensesCategory,
+            'incomeHandler' => $incomeHandler,
         ]);
     }
 
--- backend/views/chart/index.php
+++ backend/views/chart/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-use backend\widgets\Alert;
+use daixianceng\echarts\ECharts;
 
 $this->title = '统计图表';
 ?>
@@ -9,7 +9,9 @@ $this->title = '统计图表';
     <div class="col-lg-12">
         <h1 class="page-header"><?= Html::encode($this->title) ?></h1>
     </div>
-    
+</div>
+
+<div class="row">
     <div class="fms-summary col-md-12">
         <div class="fms-summary-item col-md-2 fms-summary-income">
             <div class="fms-summary-attr">总收入</div>
@@ -23,5 +25,54 @@ $this->title = '统计图表';
             <div class="fms-summary-attr">结余</div>
             <div class="fms-summary-num"><?= ($incomeTotal-$expensesTotal) ?></div>
         </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="text-center col-md-6">
+        <h3>消费统计</h3>
+        <?= ECharts::widget([
+            'responsive' => true,
+            'options' => [
+                'style' => 'height: 400px;'
+            ],
+            'pluginOptions' => [
+                'option' => [
+                    'tooltip' => [
+                        'trigger' => 'item',
+                        'formatter' => '{a} <br/>{b}: {c} ({d}%)'
+                    ],
+                    'series' => [
+                        'name' => '金额',
+                        'type' => 'pie',
+                        'radius' => ['66%', '80%'],
+                        'data' => $expensesCategory
+                    ]
+                ]
+            ]
+        ]) ?>
+    </div>
+    <div class="text-center col-md-6">
+        <h3>存钱统计</h3>
+        <?= ECharts::widget([
+            'responsive' => true,
+            'options' => [
+                'style' => 'height: 400px;'
+            ],
+            'pluginOptions' => [
+                'option' => [
+                    'tooltip' => [
+                        'trigger' => 'item',
+                        'formatter' => '{a} <br/>{b}: {c} ({d}%)'
+                    ],
+                    'series' => [
+                        'name' => '金额',
+                        'type' => 'pie',
+                        'radius' => ['66%', '80%'],
+                        'data' => $incomeHandler
+                    ]
+                ]
+            ]
+        ]) ?>
     </div>
 </div>
